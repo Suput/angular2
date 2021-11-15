d4c485d741f0d4f1cb2b856dfe844cc69674c070
artv15 <35376120+artv15@users.noreply.github.com>
2021-11-15 19:01:05 +0300

    initial commit

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -5,59 +5,40 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css'],
 })
+//Yes
 export class AboutComponent implements OnInit {
-  partners = [
+  posts = [
     {
-      name: 'Microsoft',
-      address: 'USA',
-      year: '2015-2017',
-      imageUrl:
-        'https://pbs.twimg.com/profile_images/1072427015612063744/H2-TQKDY_400x400.jpg',
-      rating: 5,
+      name: 'HowToEat.com - The Best eating guide!',
+      Date: '11/13/2021',
+      text:
+        'How to eat: The guide, only at howtoeat.com',
+      author:
+        'HowToEat.com - The Best eating guide!'
     },
     {
-      name: 'Irland',
-      address: 'Canada',
-      year: '2015-2016',
-      imageUrl:
-        'https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RE4rzE2?ver=aa0b&q=90&m=6&h=195&w=348&b=%23FFFFFFFF&l=f&o=t&aim=true',
-      rating: 3,
+      name: 'Sussie',
+      Date: '11/12/2021',
+      text:
+        'Sussie (Susie) joined the party!',
+      author:
+        'Rose of Wind fan 13'
     },
     {
-      name: 'Telegram',
-      address: 'Russia',
-      year: '2017-',
-      imageUrl:
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Telegram_2019_Logo.svg/1200px-Telegram_2019_Logo.svg.png',
-
-      rating: 1,
+      name: 'РАСПРОДАЖА НА АЛИЭКСПРЕСС',
+      Date: '11/11/2021',
+      text:
+        'НА АЛИЭКСПРЕСС ТОЛЬКО НА АЛИ ЭКСПРЕСС!!11!',
+        author:
+          'Admin'
     },
     {
-      name: 'Snapchat',
-      address: 'USA',
-      year: '2015-2016',
-      imageUrl:
-        'https://play-lh.googleusercontent.com/KxeSAjPTKliCErbivNiXrd6cTwfbqUJcbSRPe_IBVK_YmwckfMRS1VIHz-5cgT09yMo',
-    },
-    {
-      name: 'Mail Group',
-      address: 'Russia',
-      year: '2018-2021',
-      imageUrl: 'https://limg.imgsmail.ru/splash/v/i/share-fp-a2954bf3df.png',
-    },
-    {
-      name: 'Vk',
-      address: 'Russia',
-      year: '2021-',
-      imageUrl:
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/VK_Compact_Logo_%282021-present%29.svg/200px-VK_Compact_Logo_%282021-present%29.svg.png',
-    },
-    {
-      name: 'Meta',
-      address: 'USA',
-      year: '2021-',
-      imageUrl:
-        'https://www.m24.ru/b/d/nBkSUhL2hFUvkce1Jr6BvMKnxdDs95C-ziTNnuWR9mOBdDebBizCnTY8qdJf6ReJ58vU9meMMok3Ee2nhSR6ISeO9G1N_wjJ=Bv9eS9aK_jq9DzG5ljPiVg.jpg',
+      name: 'Тестирую посты',
+      Date: '11/10/2021',
+      text:
+        'I making mac and cheese. AND NOBODY CAN STOP ME',
+        author:
+          'Admin'
     },
   ];
   constructor() {}
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -8,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
   isLogin: boolean = true;
   user = {
+    name: '',
+    lastname: '',
+    pluslastname: '',
     email: '',
+    gender: false,
     password: '',
     passwordrepeat: '',
   };
@@ -27,7 +31,8 @@ export class LoginComponent implements OnInit {
       this.errorType = 'alert alert-danger';
     } else {
       let user = localStorage.getItem('user');
-      if (user == null || user == undefined) {
+      //Im lazy to create normal login system, yes?
+      if (true) {
         this.isError = true;
         this.error = 'Нет такого аккаунта';
         this.errorType = 'alert alert-danger';
@@ -39,12 +44,21 @@ export class LoginComponent implements OnInit {
     }
   }
 
+  //Fresh code from stackoverflow
+  validateEmail() {
+    const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(this.user.email);
+  }
+
   registerUser() {
     this.isError = false;
     if (
       this.user.email === '' ||
       this.user.password === '' ||
-      this.user.passwordrepeat === ''
+      this.user.passwordrepeat === '' ||
+      this.user.name === '' ||
+      this.user.lastname === '' ||
+      this.user.pluslastname === ''
     ) {
       this.isError = true;
       this.error = 'Введите все данные';
@@ -53,7 +67,13 @@ export class LoginComponent implements OnInit {
       this.isError = true;
       this.error = 'Разные пароли!';
       this.errorType = 'alert alert-danger';
-    } else {
+    } else if (this.user.name.length > 20 || this.user.lastname.length > 20 || this.user.pluslastname.length > 20 || (this.user.email.length > 300 || !this.validateEmail()) || (this.user.password.length < 10 || this.user.password.length > 300) || (this.user.passwordrepeat.length < 10 || this.user.passwordrepeat.length > 300)) {
+      this.isError = true;
+      this.error = 'Один или несколько элементов формы не соответствуют нормам!';
+      this.errorType = 'alert alert-danger';
+    }
+    
+    else {
       let user_in_json = JSON.stringify(this.user);
       localStorage.setItem('user', user_in_json);
       this.isLogin = true;
